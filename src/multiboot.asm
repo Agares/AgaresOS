2916c9c993017d8b3ead883d7ec3135ab2b92a9a
agares <ja@agares.info>
2015-05-17 09:21:02 +0200

    Fixed multiboot header - Added required tags section - Replaced hardcoded length with simple calculation

--- src/multiboot.asm
+++ src/multiboot.asm
@@ -1,8 +1,25 @@
+%define UINT32_MAX 4294967295
+
 %define MULTIBOOT2_HEADER_MAGIC 0xe85250d6
 %define MULTIBOOT_ARCHITECTURE_I386 0
 
 section .multiboot-header
-dd MULTIBOOT2_HEADER_MAGIC
-dd MULTIBOOT_ARCHITECTURE_I386
-dd 0
-dd -(MULTIBOOT2_HEADER_MAGIC+MULTIBOOT_ARCHITECTURE_I386)
+multiboot_header:
+	; magic value
+	dd MULTIBOOT2_HEADER_MAGIC
+	; architecture (i386)
+	dd MULTIBOOT_ARCHITECTURE_I386
+	; header_length (in bytes)
+	dd multiboot_header_end - multiboot_header
+	; checksum
+	dd UINT32_MAX-( \
+		MULTIBOOT2_HEADER_MAGIC \
+		+MULTIBOOT_ARCHITECTURE_I386 \
+		+(multiboot_header_end - multiboot_header) \
+		-1 \
+	)
+multiboot_header_tag_end:
+	dw 0 ;type
+	dw 0 ;flags
+	dd 8 ;size
+multiboot_header_end:
