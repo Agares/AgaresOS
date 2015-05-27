bd24338177516c2a9e818851a7fb727aa25d2923
agares <ja@agares.info>
2015-05-27 20:32:36 +0200

    Moved part of loader's Makefile to commmon file Now it is possible to write similar Makefiles for other parts (e.g. the kernel itself), that share part of common functionality

---
+++ common.mk
@@ -0,0 +1,33 @@
+SRCDIR := src
+OBJDIR := obj
+DISTDIR := dist
+
+CFILES := $(shell find $(SRCDIR) -type f -name "*.c")
+COBJFILES := $(patsubst src/%, obj/%, $(patsubst %.c, %.o, $(CFILES)))
+
+ASMFILES := $(shell find $(SRCDIR) -type f -name "*.asm")
+ASMOBJFILES := $(patsubst src/%, obj/%, $(patsubst %.asm, %.a.o, $(ASMFILES)))
+
+OBJFILES := $(COBJFILES) $(ASMOBJFILES)
+
+WARNINGFLAGS := -Wall -Wextra -pedantic -Wshadow -Wpointer-arith -Wcast-align \
+	-Wwrite-strings -Wmissing-prototypes -Wmissing-declarations \
+	-Wredundant-decls -Wnested-externs -Winline -Wno-long-long \
+	-Wuninitialized -Wconversion -Werror
+
+CFLAGS := -fno-asynchronous-unwind-tables -m32 -nostdlib -ffreestanding -std=c11 $(WARNINGFLAGS)
+LDFLAGS := -melf_i386 -static -L ./
+ASMFLAGS := -felf32
+ASM = nasm
+
+clean:
+	find $(OBJDIR) -not -name ".*" -type f -delete
+	find $(DISTDIR) -not -name ".*" -type f -delete
+
+$(OBJDIR)/%.a.o: $(SRCDIR)/%.asm
+	mkdir -p $(@D)
+	$(ASM) $(ASMFLAGS) $< -o $@
+
+$(OBJDIR)/%.o: $(SRCDIR)/%.c 
+	mkdir -p $(@D)
+	$(CC) $(CFLAGS) -c $< -o $@
