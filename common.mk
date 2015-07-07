7516d1e4a5d9c123201409e78fc9ddd36bb50726
Agares <ja@agares.info>
2015-07-08 00:05:35 +0200

    Added possibility to share C code
    The code can be shared between kernel and loader

--- common.mk
+++ common.mk
@@ -1,6 +1,11 @@
+COMMONDIR := ../common
+
 SRCDIR := src
 OBJDIR := obj
 DISTDIR := dist
+
+COMMONSRCDIR := $(COMMONDIR)/$(SRCDIR)
+COMMONOBJDIR := $(COMMONDIR)/$(OBJDIR)/$(ARCH)
 
 CFILES := $(shell find $(SRCDIR) -type f -name "*.c")
 COBJFILES := $(patsubst src/%, obj/%, $(patsubst %.c, %.o, $(CFILES)))
@@ -8,7 +13,10 @@ COBJFILES := $(patsubst src/%, obj/%, $(patsubst %.c, %.o, $(CFILES)))
 ASMFILES := $(shell find $(SRCDIR) -type f -name "*.asm")
 ASMOBJFILES := $(patsubst src/%, obj/%, $(patsubst %.asm, %.a.o, $(ASMFILES)))
 
-OBJFILES := $(COBJFILES) $(ASMOBJFILES)
+COMMONCFILES := $(shell find $(COMMONSRCDIR) -type f -name "*.c")
+COMMONCOBJFILES := $(patsubst $(COMMONSRCDIR)/%, $(COMMONOBJDIR)/%, $(patsubst %.c, %.o, $(COMMONCFILES)))
+
+OBJFILES := $(COBJFILES) $(ASMOBJFILES) $(COMMONCOBJFILES)
 
 WARNINGFLAGS := -Wall -Wextra -pedantic -Wshadow -Wpointer-arith -Wcast-align \
 	-Wwrite-strings -Wmissing-prototypes -Wmissing-declarations \
@@ -25,6 +33,10 @@ ASM = nasm
 clean:
 	find $(OBJDIR) -not -name ".*" -type f -delete
 	find $(DISTDIR) -not -name ".*" -type f -delete
+
+$(COMMONOBJDIR)/%.o: $(COMMONSRCDIR)/%.c
+	mkdir -p $(@D)
+	$(CC) $(CFLAGS) -c $< -o $@
 
 $(OBJDIR)/%.a.o: $(SRCDIR)/%.asm
 	mkdir -p $(@D)
