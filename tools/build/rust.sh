72a2cf10d7fe01d17177115ca7d1d65c3013160d
Agares <ja@agares.info>
2015-08-01 20:17:03 +0200

    Changed rust's prefix to local/

--- tools/build/rust.sh
+++ tools/build/rust.sh
@@ -6,6 +6,6 @@ cd build/cross/rustc/
 wget https://static.rust-lang.org/dist/rustc-nightly-src.tar.gz
 tar zxf rustc-nightly-src.tar.gz
 cd rustc-nightly
-./configure --prefix=$PROJECT_DIR/local/compiler/rustc
+./configure --prefix=$PROJECT_DIR/local/
 make
 make install
